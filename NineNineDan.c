6f0e605522ddd75a3bc21ca43291a0c966a614ba
ZIBA99 <97379804+ZIBA99@users.noreply.github.com>
2025-04-24 18:20:21 +0900

    Update NineNineDan.c

--- NineNineDan.c
+++ NineNineDan.c
@@ -1,15 +1,15 @@
-//#include <stdio.h>
-//
-//int main(void)
-//{
-//	int dan = 0, num = 1;
-//	printf("¸î ´Ü? ");
-//	scanf_s("%d", &dan);
-//
-//	while (num < 10)
-//	{
-//		printf("%d x %d=%d \n", dan, num, dan * num);
-//		num++;
-//	}
-//	return 0;
-//}
+#include <stdio.h>
+
+int main(void)
+{
+	int dan = 0, num = 1;
+	printf("Â¸Ã® Â´Ãœ? ");
+	scanf_s("%d", &dan);
+
+	while (num < 10)
+	{
+		printf("%d x %d=%d \n", dan, num, dan * num);
+	num++;
+	}
+	return 0;
+}
